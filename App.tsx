801b23c724c681e11a0864bad2b20b1e52229925
Johan Tovar <jtovarto@gmail.com>
2023-05-18 16:08:18 -0400

    implement ble manager in app screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Screen1} from './src/screen/Screen1';
@@ -6,10 +6,115 @@ import {Screen2} from './src/screen/Screen2';
 import {Screen3} from './src/screen/Screen3';
 import {Screen4} from './src/screen/Screen4';
 
+import BleManager, {Peripheral} from 'react-native-ble-manager';
+import {
+  NativeEventEmitter,
+  NativeModules,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
+import useDevices from './src/hooks/useDevices';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const {addDiscoveredDevice} = useDevices();
+
+  const handleDiscoverPeripheral = ({name, id}: Peripheral) => {
+    if (
+      [
+        'UNIT1 FARO',
+        'U1SMARTLIGHT',
+        'NAVIGATION-REMOTE',
+        'STROMER SMART H',
+      ].includes(name)
+    ) {
+      addDiscoveredDevice({
+        name: name,
+        address: id,
+        isSaved: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    try {
+      BleManager.start({showAlert: false})
+        .then(() => console.debug('BleManager started.'))
+        .catch(error =>
+          console.error('BeManager could not be started.', error),
+        );
+    } catch (error) {
+      console.error('unexpected error starting BleManager.', error);
+      return;
+    }
+
+    const listeners = [
+      bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        handleDiscoverPeripheral,
+      ),
+    ];
+
+    handleAndroidPermissions();
+
+    return () => {
+      console.debug('[app] main component unmounting. Removing listeners...');
+      for (const listener of listeners) {
+        listener.remove();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleAndroidPermissions = () => {
+    if (Platform.OS === 'android' && Platform.Version >= 31) {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      ]).then(result => {
+        if (result) {
+          console.debug(
+            '[handleAndroidPermissions] User accepts runtime permissions android 12+',
+          );
+        } else {
+          console.error(
+            '[handleAndroidPermissions] User refuses runtime permissions android 12+',
+          );
+        }
+      });
+    } else if (Platform.OS === 'android' && Platform.Version >= 23) {
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ).then(checkResult => {
+        if (checkResult) {
+          console.debug(
+            '[handleAndroidPermissions] runtime permission Android <12 already OK',
+          );
+        } else {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ).then(requestResult => {
+            if (requestResult) {
+              console.debug(
+                '[handleAndroidPermissions] User accepts runtime permission android <12',
+              );
+            } else {
+              console.error(
+                '[handleAndroidPermissions] User refuses runtime permission android <12',
+              );
+            }
+          });
+        }
+      });
+    }
+  };
+
   console.log('RENDER APP TSX');
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Screen3">

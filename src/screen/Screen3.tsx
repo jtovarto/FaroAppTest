77cb405c809e57d54fb0c05af2b727c29a3764c8
Johan Tovar <jtovarto@gmail.com>
2023-05-18 16:07:42 -0400

    add button scan to screen 3

--- src/screen/Screen3.tsx
+++ src/screen/Screen3.tsx
@@ -2,15 +2,50 @@ import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {DeviceContainer} from '../components/DeviceContainer';
 import {useNavigation} from '@react-navigation/native';
+import BleManager, {
+  BleScanCallbackType,
+  BleScanMatchMode,
+  BleScanMode,
+} from 'react-native-ble-manager';
+
+const SECONDS_TO_SCAN_FOR = 7;
+const SERVICE_UUIDS: string[] = [];
+const ALLOW_DUPLICATES = true;
 
 export const Screen3 = () => {
   const navigation = useNavigation();
+  // const updateDiscovered = useSetAtom(discovered);
+
+  const startScan = () => {
+    // reset found peripherals before scan
+    // updateDiscovered(new Map<Peripheral['id'], Peripheral>());
+
+    try {
+      console.debug('[startScan] starting scan...');
+      BleManager.scan(SERVICE_UUIDS, SECONDS_TO_SCAN_FOR, ALLOW_DUPLICATES, {
+        matchMode: BleScanMatchMode.Sticky,
+        scanMode: BleScanMode.LowLatency,
+        callbackType: BleScanCallbackType.AllMatches,
+      })
+        .then(() => {
+          console.debug('[startScan] scan promise returned successfully.');
+        })
+        .catch(err => {
+          console.error('[startScan] ble scan returned in error', err);
+        });
+    } catch (error) {
+      console.error('[startScan] ble scan error thrown', error);
+    }
+  };
 
   console.log('RENDER SCREEN 3');
 
   return (
     <View style={styles.screen}>
       <DeviceContainer />
+      <TouchableOpacity style={styles.buttonScan} onPress={startScan}>
+        <Text style={styles.label}>Start Scann</Text>
+      </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Screen4')}>
@@ -24,7 +59,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
-
   button: {
     backgroundColor: 'blue',
     padding: 20,
@@ -34,6 +68,16 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     position: 'absolute',
     bottom: '5%',
+  },
+  buttonScan: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    width: '90%',
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: '14%',
   },
   label: {
     color: 'white',

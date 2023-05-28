919f5f60a7980d58f068f4cabc866afb08d98465
Johan Tovar <jtovarto@gmail.com>
2023-05-28 11:15:17 -0400

    update ble store

--- src/stores/BleStore.ts
+++ src/stores/BleStore.ts
@@ -1,39 +1,72 @@
 import {atom} from 'jotai';
-import {Devices} from '../types/types';
+import {Device, Devices} from '../types/types';
 
-// const bleStore = createStore();
+const paired = atom<Devices>(new Map());
+const discovered = atom<Devices>(new Map());
 
-const devicesHandler = (devices: any) => {
-  return;
-};
+export enum ACTIONS {
+  ADD = 'add',
+  REMOVE = 'remove',
+  UPDATE = 'update',
+}
 
-export const paired = atom<Devices>(
-  new Map(),
-  (get: any, set: any, {action, device}: any) => {
-    const previousDevices = get(paired);
-    if (action === 'remove') {
-      previousDevices.delete(device.address);
-    } else if (action === 'add') {
-      previousDevices.set(device.address, device);
+export type Payload =
+  | {
+      action: ACTIONS.ADD | ACTIONS.UPDATE;
+      device: Partial<Device> & {address: string};
     }
-    set(paired, new Map(previousDevices));
+  | {
+      action: ACTIONS.REMOVE;
+      device: string;
+    };
+
+export const pairedDevices = atom(
+  get => get(paired),
+  (get, set, {action, device}) => {
+    const devices = new Map(get(paired));
+
+    switch (action) {
+      case ACTIONS.ADD:
+        if (devices.has(device.address)) return;
+        devices.set(device.address, device);
+        break;
+      case ACTIONS.REMOVE:
+        if (!devices.has(device)) return;
+        console.log('remove', device);
+        devices.delete(device.address);
+        break;
+      case ACTIONS.UPDATE:
+        if (!devices.has(device.address)) return;
+        devices.set(device.address, device);
+        break;
+      default:
+        break;
+    }
+
+    console.log('pairedDevices', action, device);
+    set(paired, devices);
   },
 );
 
-export const discovered = atom<Devices>(
-  new Map(),
-  (get: any, set: any, {action, device}: any) => {
+export const discoveredDevices = atom(
+  get => get(discovered),
+  (get, set, {action, device}) => {
     const previousDevices = get(discovered);
+
+    if (previousDevices?.has(device.address)) {
+      return;
+    }
+
     if (action === 'remove') {
-      previousDevices.delete(device.address);
+      previousDevices?.delete(device.address);
     } else if (action === 'add') {
-      previousDevices.set(device.address, device);
+      previousDevices?.set(device.address, device);
     }
     set(discovered, new Map(previousDevices));
   },
 );
 
 export default {
-  paired,
-  discovered,
+  pairedDevices,
+  discoveredDevices,
 };
